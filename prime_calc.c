6b4ea76df7ec5fcf27ca5368ce4cea96359147b9
Gaetan Longree <gaetanlongree@gmail.com>
2019-06-20 20:54:42 +0200

    Add files via upload

---
+++ prime_calc.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <time.h>
+
+void main()
+{
+    FILE * file = fopen("output.txt", "w");
+    int n=300000, i, fact, j;
+    clock_t start, end;
+    double cpu_time_used = 0.;
+    fprintf(file, "Prime numbers are: \n");
+    start = clock();
+    for(i=1; i<=n; i++)
+    {
+        fact=0;
+        for(j=1; j<=n; j++)
+        {
+            if(i%j==0)
+                fact++;
+        }
+        if(fact==2)
+            fprintf(file, "%d\n" ,i);
+    }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    fprintf(file, "Total time taken: %f\n", cpu_time_used);
+    getchar();
+}
